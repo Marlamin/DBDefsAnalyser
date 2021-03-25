e89ab6b7de52ae85e741f60759c52c28f33abb08
barncastle <spearman.tom@gmail.com>
2021-03-25 10:21:30 +0000

    misc

--- DBDefsAnalyser/Utils/WebClientEx.cs
+++ DBDefsAnalyser/Utils/WebClientEx.cs
@@ -11,6 +11,7 @@ namespace DBDefsAnalyser.Utils
         {
             var request = (HttpWebRequest)base.GetWebRequest(address);
             request.AutomaticDecompression = AutomaticDecompression;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57";
             return request;
         }
     }
